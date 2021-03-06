2dbfed7384a7f0938b4a094e352756b5c2451c3c
ziyueshiyun <68774427+ziyueshiyun@users.noreply.github.com>
2021-03-06 15:36:49 +0800

    Add files via upload

---
+++ Colored_Points.m
@@ -0,0 +1,8 @@
+function [  ] = Colored_Points()
+% 新建一个函数，运行这个函数后可以得到这4800个点所代表的周期轨道类型，并用
+% 不同的颜色标注出在Xinit,Dyinit二维平面图上
+% 函数名叫Colored_Points
+
+
+end
+
--- Grid_Search.m
+++ Grid_Search.m
@@ -1,10 +1,14 @@
-function [X_half] = Grid_Search(M)
-% Grid_Search 根据生成的矩阵M，就是1330x6的那个矩阵获得第一次y坐标归为0
+function [X_half] = Grid_Search(M,Row)
+% Grid_Search 根据生成的矩阵M，以及矩阵的行数Row
+% 获得第一次y坐标归为0
 % 时刻的x,y,z,dx,dy,dz 以及步长，这里为方便后面的绘图，直接转换为常用时间
 % 和长度单位
-
-
-
-
+X_half = [0,0,0,0,0,0];
+for i = 1:Row
+    if M(Row,2)*M(Row+1,2) < 0
+        X_half = M(Row,:);
+        break;
+    end
+end
 end
 
