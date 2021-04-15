f1b31556deed3bfb9fddd579f31beda4b5ae577a
ziyueshiyun <68774427+ziyueshiyun@users.noreply.github.com>
2021-04-15 16:38:38 +0800

    Add files via upload

--- Colored_Points.m
+++ Colored_Points.m
@@ -3,46 +3,53 @@ function [XOYPot] = Colored_Points()
 % 不同的颜色标注出在Xinit,Dyinit二维平面图上
 % 函数名叫Colored_Points
 
+% 时间单位m
 Tu = 6.5e6;
+% 长度单位s
 Lu = 1.1e5;
 % 确定二维图形的x坐标和y坐标范围取点数为60 x 80
-% 其中x坐标代表初始x坐标，单位为m y坐标代表初始y速度，单位为cm/s
-xLabel = 60000;
-yLabel = 80;
+% x最大值为30km
+xMax = 30;
 
-% 用一个4800行4列的矩阵来存储x坐标，y坐标以及对应周期，速度变化量cm/s
+% dy最大值为40cm/s
+yMax = 40;    
+
+% 用一个n行4列的矩阵来存储x坐标，y坐标以及对应周期，速度变化量cm/s
 % Row = xLabel*yLabel;
 
-XOYPot = zeros(56,4);
+XOYPot = zeros(1,5);
+Row = 1;
 % 初始仿真时间设置为0，结束仿真时间设置为T_term 假设整个周期最长为20天
 
-% 仿真持续时间为半个周期长度
-t_term = 100*24*3600/Tu;
+% 仿真持续时间20天
+t_term = 20*24*3600/Tu;
 
-% 每个点使用ode45函数所生成的矩阵行数
-% OdeRow = t_term / 0.001;
 
-% 统计新生成的矩阵的行数
-Row = 1;
-        
-for Xinit = -xLabel/2 :10000: xLabel/2
-    for Dyinit = -yLabel/2 :10: yLabel/2
+
+% 注意，此处要排除Xinit = 0 的点，因为在该点位置与小天体碰撞了
+for Xinit = -xMax:0.1:xMax
+    if Xinit == 0; continue;
+    end
+    for Dyinit = -yMax:0.1:yMax
+
         
         % 对于坐标为x0,Dy0的点先对单位进行归一化
-        x0 = Xinit/Lu;
-        Dy0 = Dyinit/100/Lu/(1/Tu);
+        x0 = 1000*Xinit/Lu;
+        Dy0 = Dyinit/100/(Lu/Tu);
         
         % 用ode45工具对方程进行积分，确定积分步长及其意义
         X0 = [x0,0,0,0,Dy0,0];
         
         
         % 将仿真时间段进行单位化
-        [t,x] = ode45(@DynamicEq02,[0 t_term],X0);
+        op = odeset('Events',@EventFun);
+        [t,x,Tend,Xend,evenum] = ode45(@DynamicEq02,[0,t_term],X0,op);
         
-        [Xhalf,Time_Needed] = Grid_Search(x,t);
+        Xhalf = x(end,:);
+        
         
         % 还原时间单位为天
-        Time_Needed = abs(2*Time_Needed*Tu/3600/24);
+        Time_Needed = 2*Tend(end)*Tu/3600/24;
         % 还原速度单位为米每秒
         Dy = Xhalf(5)*Lu/Tu;
         
@@ -51,9 +58,17 @@ for Xinit = -xLabel/2 :10000: xLabel/2
         XOYPot(Row,2) = Dyinit;
         XOYPot(Row,3) = Time_Needed;
         XOYPot(Row,4) = Dy;
+        % xtermal
+        XOYPot(Row,5) = Xhalf(1)*Lu;
         Row = Row+1;
+        Row
     end
 end
+
+
+% x_init = -xLabel/2 :10000: xLabel/2;
+% dy_init= -yLabel/2 :10: yLabel/2;
+% [Xinit,Dyinit] = meshgrid(x_init,dy_init);
 
 
 end
